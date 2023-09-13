477b2409afc8c414bfa503815a39c129eb4a936c
dtd217 <duydong217@gmail.com>
2023-09-13 17:30:30 +0700

    cm-

--- backend/server.js
+++ backend/server.js
@@ -18,14 +18,7 @@ connectDatabase();
 const app = express();
 
 // Middlewares
-// app.use(cors());
-const corsConfig = {
-    origin: '',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
-}
-app.use(cors(corsConfig))
-app.options("", cors(corsConfig))
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 

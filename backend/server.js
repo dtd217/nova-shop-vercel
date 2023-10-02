c783e4ddc254a461f4dcab17dd0bec31bbfe0346
dtd217 <duydong217@gmail.com>
2023-10-02 22:10:24 +0700

    cm--

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
+import path from "path";
 
 // Configure env
 dotenv.config();
@@ -18,11 +19,10 @@ connectDatabase();
 const app = express();
 
 // Middlewares
-app.use(cors({
-    origin: "https://nova-shop-vercel.vercel.app/"
-}));
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 // Routes
 app.use("/api/v1/auth", authRoutes);
@@ -32,6 +32,10 @@ app.use("/api/v1/product", productRoutes);
 // Rest API
 app.get('/', (req, res) => {
     res.send(`<h1>Hello World</h1>`)
+})
+
+app.use("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 })
 
 // PORT
